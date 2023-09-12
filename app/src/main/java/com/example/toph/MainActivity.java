6254a658719f83706dfe65f4da09aae2fad0d9b0
Jonathan López <jolopezn@unal.edu.co>
2023-09-11 19:22:18 -0500

    Primer commit

---
+++ app/src/main/java/com/example/toph/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.toph;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.Base_Theme_Toph);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.log_in);
+
+    }
+    public void IniciarSesion (View view){
+
+        TextView correo = (TextView) findViewById(R.id.correo);
+        TextView contraseña = (TextView) findViewById(R.id.contraseña);
+        Button boton = (Button) findViewById(R.id.IniciarSesion);
+
+        if(correo.getText().toString().toLowerCase().equals("admin@gmail.com") && contraseña.getText().toString().equals("admin123")){
+            Toast.makeText(this,"Inicio de sesion exitosa", Toast.LENGTH_LONG).show();
+            setContentView(R.layout.activity_main);
+        }else
+            Toast.makeText(this,"Correo o Contraseña Incorrectos", Toast.LENGTH_LONG).show();
+
+
+    }
+}
